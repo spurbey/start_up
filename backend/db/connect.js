bd59afe54c2900ef7ef414a6ac414520b9d4da73
deepak878 <deepakbhattarai878@gmail.com>
2023-05-29 13:06:02 +0545

    env setup

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 export const connect = async ()=>{
   try {
+    console.log(process.env.MONGO_URI)
 await mongoose.connect(process.env.MONGO_URI)
 console.log('MongoDB working');
   // Start using the database
